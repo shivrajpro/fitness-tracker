440188280faae54b12364907f11406506373b801
shivrajpro <shivrajhulbatte@gmail.com>
2022-11-11 14:46:33 +0530

    using snapshot changes

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Exercise } from 'src/app/auth/models/exercise.model';
 import { TrainingService } from '../training.service';
 import { AngularFirestore } from "@angular/fire/compat/firestore";
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
@@ -11,14 +11,27 @@ import { Observable } from 'rxjs';
 })
 export class NewTrainingComponent implements OnInit {
   @Output()  trainingStart = new EventEmitter<void>();
-  exercises!:Observable<any>;
+  exercises!:Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService,
     private db:AngularFirestore) { }
 
   ngOnInit(): void {
     // this.exercises = this.trainingService.getAvailableExercises();
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    // this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.exercises = this.db.collection('availableExercises').snapshotChanges()
+    .pipe(map(docArray=>{
+      return docArray.map(doc=>{
+        const data: Exercise = doc.payload.doc.data() as Exercise;
+
+        return {
+          id: doc.payload.doc.id,
+          name: data.name,
+          duration:data.duration,
+          calories: data.calories
+        }
+      })
+    }))
   }
 
   onStartTraining(form:NgForm){
